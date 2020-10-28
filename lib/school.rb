bba01b0818c9bcc43be3745f2d267d2e9eee64ef
https://learn.co/DenaNeuman <denaweiss5@gmail.com>
2020-10-27 22:05:55 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,31 +1,8 @@
-class School
-    def initialize(school_name)
-        @school_name=school_name
-        @roster = {}
-    end
-
-    def roster
-        roster = {}
-    end
-   
-    def add_student(student_name, grade)
-        @student_name=student_name
-        @grade=grade
-    end
-
-    def grade(grade)
-        if @grade==grade 
-             @student_name
-        end
-
-    end
-
-end
 
 
 class School
     attr_accessor :school_name, :roster
-    
+
     def initialize (school_name)
         @school_name = school_name
         @roster = {}
